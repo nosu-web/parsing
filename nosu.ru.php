06dfd2849655058fb8f8d01ae5479a48b0d101a7
NOSU-WEB <79894331+nosu-web@users.noreply.github.com>
2022-03-17 10:54:30 +0300

    Update nosu.ru.php

--- nosu.ru.php
+++ nosu.ru.php
@@ -8,18 +8,31 @@ $doc->loadHTML($html);
 /* Класс DOMXpath реализует язык запросов XPath к элементам XML-документа */
 $xpath = new DOMXpath($doc);
 
+$img_full_path = '/uploads/';
+$img_tmb_path = '/uploads/thumbnails';
+
 /* Находим все контейнеры div с классом item (карточки новостей) в контейнере div склассом news-list */
 foreach ($xpath->query("//div[contains(@class, 'news-list')]//div[contains(@class, 'item')]") as $item) {
   /* Находим DOM-элемент заголовка */
   $title = $xpath->query(".//div[contains(@class, 'title')]//a", $item);
   /* Получаем текстовое содержимое заголовка */
   $title_text = $title[0]->textContent;
+  /* Получаем дату новости */
+  $date = $xpath->query(".//div[contains(@class, 'date')]", $item);
+  $date_text = $date[0]->textContent;
 
   /* Находим DOM-элемент изображения */
   $image = $xpath->query(".//a//img", $item);
   /* Если элемент не пустой получаем значение атрибута src */
   if($image[0] !== null)
-    $image_src = $image[0]->getAttribute('src');
+    $image_tmb = $image[0]->getAttribute('src'); // Ссылка на миниатюру
+
+  $image_tmb_binary = file_get_contents($image_tmb);
+  echo $image_tmb_binary;
+
+  $image_full = str_replace('-350x230', '', $image_tmb);
   
-  echo "{$title_text} - {$image_src}<br>";
+  echo "<h3>{$title_text}</h3>";
+  echo "<date>{$date_text}<date>";
+  echo "<div><img src=\"{$image_tmb}\"></div>";
 }
