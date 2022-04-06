abd985e501ed45de6d748f9706ce30edaa1947b7
NOSU-WEB <79894331+nosu-web@users.noreply.github.com>
2022-04-06 10:32:04 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,89 @@
+<?php
+/* Подключаем файл с соединением к БД */
+include("includes/mysql.inc.php");
+
+/* Выбираем 10 последних новостей из таблицы news */
+$result = $mysqli->query("SELECT * FROM `news` ORDER BY `date` DESC LIMIT 10");
+
+if(isset($_POST["submit"])) {
+    $search_phrase = $_POST["search_phrase"];
+
+    $result = $mysqli->query("SELECT * FROM `news`
+    WHERE
+        `title` LIKE '%{$search_phrase}%'
+    OR 
+        `text` LIKE '%{$search_phrase}%'
+    ORDER BY
+        `date` DESC LIMIT 10");
+}
+
+$news = '';
+while ($row = $result->fetch_assoc()) {
+
+    $title = $row['title'];
+    $date = $row['date'];
+    $text = $row['text'];
+    $img = $row['img'];
+    $url = $row['url'];
+    
+    $text = mb_substr($text, 0, 100).'...';
+
+    $news .= "
+    <div class=\"card my-3\">
+        <!--<img src=\"{$img}\" class=\"card-img-top\" alt=\"{$title}\">-->
+        <div class=\"card-body\">
+            <h5 class=\"card-title\">{$title}</h5>
+            <p class=\"card-text\">{$text}</p>
+            <a href=\"{$url}\" class=\"btn btn-primary\" target=\"_blank\">Подробнее</a>
+        </div>
+    </div>";
+}
+?>
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Новости Осетии</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">Новости Осетии</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/">Новости</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">О проекте</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" action="" method="post">
+                        <input class="form-control me-2" name="search_phrase" type="search" placeholder="Введите запрос" aria-label="Найти">
+                        <button class="btn btn-outline-success" name="submit" type="submit">Найти</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main class="mt-3">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <?=$news;?>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
