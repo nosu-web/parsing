7b57084720cb0a7d32a57495bd14cc3e7ed3ac09
NOSU-WEB <79894331+nosu-web@users.noreply.github.com>
2022-05-04 09:39:54 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,17 +1,26 @@
 <?php
 session_start();
+/* Подключаемся к БД */
+include('includes/mysql.inc.php');
+
+
 if(isset($_POST['submit']))
 {
     $login = $_POST['login'];
     $password = $_POST['password'];
 
-    if($login == 'admin' && $password == '12345678')
-    {
-        $_SESSION['is_user'] = true;
-        header("Location: index.php");
+    /* Запросы SELECT, возвращают набор результатов */
+    $result = $mysqli->query("SELECT password FROM `users` WHERE `login`='{$login}'");
+    while($row = $result->fetch_assoc()) {
+        if(password_verify($password, $row['password']))
+        {
+            $_SESSION['is_user'] = true;
+            header("Location: index.php");
+        }
     }
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="ru">
 
@@ -23,17 +32,18 @@ if(isset($_POST['submit']))
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <div class="conatiner">
-        <form method="post" action="login.php">
+    <div class="container py-5">
+        <form method="post" action="login.php" class="w-25 mx-auto">
+            <h1>Авторизация</h1>
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Логин</label>
-                <input type="text" name="login" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <input type="text" name="login" class="form-control" id="login" aria-describedby="emailHelp">
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Пароль</label>
-                <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+                <input type="password" name="password" class="form-control" id="password">
             </div>
-            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" name="submit" class="btn btn-primary">Вход</button>
         </form>
     </div>
 </body>
