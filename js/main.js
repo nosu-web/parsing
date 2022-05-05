dde619d234e686230fa701e186f64b040d4796bf
NOSU-WEB <79894331+nosu-web@users.noreply.github.com>
2022-05-05 11:28:56 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $( document ).ready(function() {
+
+    //console.log(document.cookie);
+    let current_theme = getCookieValueByName('theme');
+    if(current_theme == 'navbar-dark bg-dark') {
+        $('#swicthTheme').prop('checked', true);
+    }
+
     $('#swicthTheme').change(function() {
         let theme_classes = '';
         if ($('#swicthTheme').is(":checked"))
@@ -17,3 +24,13 @@ $( document ).ready(function() {
         document.cookie = "theme=" + theme_classes + "; expires=Fri, 31 Dec 2100 23:59:59 GMT";
     });
 });
+
+function getCookieValueByName(cookieName) {
+    let cookie = {};
+    document.cookie.split(';').forEach(function(el) {
+      let [key,value] = el.split('=');
+      cookie[key.trim()] = value;
+    })
+    console.log(cookie);
+    return cookie[cookieName];
+  }
